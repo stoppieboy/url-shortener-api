3af6eeffc01b9160073bd37d834a4e03653c4a16
Shivam <shivamv1332@gmail.com>
2024-03-19 12:07:10 +0530

    api page added

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ require("dotenv").config()
 
 const express = require("express")
 const app = express();
-
-app.use(express.json())
-
+const path = require('path')
 const crypto = require('crypto')
 const pool = require('./db');
+const limiter = require('express-rate-limit')({
+    windowMs: 1*60*1000,
+    max: 200,
+})
 
-app.get('/:short_url', async (req, res) => {
+app.use(limiter);
+app.use(express.static(__dirname+'/public'))
+app.use(express.json())                         // necessary to extract body data in POST requests
+app.use(express.urlencoded({extended: true}))   // necessary to take form data from the ejs template
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+
+
+const API_URL = "/create-url";
+
+
+
+app.get('/fetch/:short_url', async (req, res) => {
     const {short_url} = req.params;
 
     const result = await pool.query('SELECT long_url FROM url_map WHERE short_url=$1;', [short_url]) 
-    console.log(result.rows);
+    console.log('fetch endpoint: ',result.rows);
     
     const long_url = result.rows[0].long_url;
 
@@ -23,12 +39,19 @@ app.get('/:short_url', async (req, res) => {
     }
 })
 
-app.post('/create-url', async (req, res) => {
+app.get('/api/create-url', (req, res) => {
+    res.render('get-url-page', {
+        title: "Page",
+        api_url: API_URL,
+    });
+})
+
+app.post(API_URL, async (req, res) => {
     const { url } = req.body;
     const hashed_url = crypto.createHash('sha1').update(url).digest('hex');
     const key = `tiny${hashed_url.substring(hashed_url.length-7)}`;
-    const result = await pool.query('INSERT INTO url_map VALUES($1, $2);',[key, url]);
-    console.log(result.rows);
+    const result = await pool.query('INSERT INTO url_map VALUES($1, $2) on conflict(short_url) do nothing;',[key, url]);
+    console.log('create url endpoint: ',result.rows);
     res.send(`This is your key: ${key}`);
 })
 
