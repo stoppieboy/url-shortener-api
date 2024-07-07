908a46043c7c6f9d968fafbe744df9b8b59feb86
Shivam Verma <shivamv1332@gmail.com>
2024-07-07 12:16:50 +0530

    code optimization

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
+// library requires
 require("dotenv").config()
-
 const express = require("express")
-const app = express();
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 const path = require('path')
+
+// custom requires
 const limiter = require('express-rate-limit')({
-    windowMs: 1*60*1000,
+    windowMs: 1 * 60 * 1000,
     max: 200,
 })
-
-app.use(limiter);
-app.use(express.static(__dirname+'/public'))
-app.use(express.json())                         // necessary to extract body data in POST requests
-app.use(express.urlencoded({extended: true}))   // necessary to take form data from the ejs template
-
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "ejs")
-
-const SHORTENING_API_ENDPOINT = "/create-url";
-const REDIRECTION_API_ENDPOINT = "/fetch";
-
 const shorteningController = require('./controllers/shorteningController')
 const redirectionController = require('./controllers/redirectionController')
 
+// constants
+const app = express();
+const SHORTENING_API_ENDPOINT = "/create-url";
+const REDIRECTION_API_ENDPOINT = "/fetch";
+const PORT = process.env.PORT || 3000
+
+// middlewares
+app.use(cors())
+app.use(helmet())
+app.use(morgan('dev'))
+app.use(limiter);
+app.use(express.static(__dirname + '/public'))
+app.use(express.json())                         // necessary to extract body data in POST requests
+app.use(express.urlencoded({ extended: true }))   // necessary to take form data from the ejs template
+
+// views
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 
 // ---------- ROUTES ------------
 
@@ -41,6 +51,4 @@ app.get('/api/test', (req, res) => {
     res.status(201).send("api online");
 })
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, ()=>console.log(`server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
