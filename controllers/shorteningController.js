b6d93b4d5a0460100b3a5ce365225b6516cfa6ea
Shivam Verma <shivamv1332@gmail.com>
2024-07-08 23:00:58 +0530

    testing

--- controllers/shorteningController.js
+++ controllers/shorteningController.js
@@ -4,19 +4,23 @@ const crypto = require('crypto')
 const pool = require('../db');
 
 router.post("/", async (req, res) => {
-    const { url } = req.body;
-    hashed_url = crypto.createHash('shake256',{outputLength: 5}).update(url).digest('base64url');
-    const result = await pool.query('INSERT INTO url_map(short_url, long_url) VALUES($1, $2) on conflict(short_url) do nothing;',[hashed_url, url]);
-
-    // reset the sequence in case of conflict due to duplicate entry in the database.
-    if(result.rows.length == 0)
-        await pool.query('select setval($1, MAX(id)) from url_map;', ['url_map_id_seq']);
-
-    res.render('get-url-page', {
-        title: "Get your Mini URL",
-        api_url: null,
-        key: hashed_url
-    });
+    try{
+        const { url } = req.body;
+        hashed_url = crypto.createHash('shake256',{outputLength: 5}).update(url).digest('base64url');
+        const result = await pool.query('INSERT INTO url_map(short_url, long_url) VALUES($1, $2) on conflict(short_url) do nothing;',[hashed_url, url]);
+    
+        // reset the sequence in case of conflict due to duplicate entry in the database.
+        if(result.rows.length == 0)
+            await pool.query('select setval($1, MAX(id)) from url_map;', ['url_map_id_seq']);
+    
+        res.render('get-url-page', {
+            title: "Get your Mini URL",
+            api_url: null,
+            key: hashed_url
+        });
+    }catch(err){
+        res.send('error occured in shortening controller')
+    }
 })
 
 module.exports = router
